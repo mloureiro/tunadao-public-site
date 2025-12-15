53f938644aefaf23edffd45ab24fffa302442034
Marcos Loureiro <mloureiro.dev@gmail.com>
2025-12-15 17:47:53 +0000

    Fix base path for local dev vs GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // Update this to your custom domain when configured
-  site: process.env.CF_PAGES_URL || 'https://tunadao.ipv.pt',
+  site: 'https://mloureiro.github.io',
+  base: process.env.BASE_PATH || '/',
 });
