7f7c4910db6e2185a3bd7222b0a7b4cd4b871a06
Marcos Loureiro <mloureiro.dev@gmail.com>
2025-12-15 10:13:20 +0000

    Update site config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mloureiro.github.io',
-  base: '/tunadao-public-site',
+  // Update this to your custom domain when configured
+  site: process.env.CF_PAGES_URL || 'https://tunadao.ipv.pt',
 });
