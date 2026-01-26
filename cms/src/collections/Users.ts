34ff2d13b5507e60bdaadbeb7cead162f558e825
Marcos Loureiro <mloureiro.dev@gmail.com>
2026-01-26 08:40:38 +0000

    fix: user authorisation to self

--- cms/src/collections/Users.ts
+++ cms/src/collections/Users.ts
@@ -18,10 +18,11 @@ export const Users: CollectionConfig = {
     delete: ({ req: { user } }) => user?.role === 'admin',
     // Users can read their own data, admins can read all
     read: ({ req: { user } }) => {
-      if (user?.role === 'admin') return true;
+      if (!user) return false;
+      if (user.role === 'admin') return true;
       return {
         id: {
-          equals: user?.id,
+          equals: user.id,
         },
       };
     },
